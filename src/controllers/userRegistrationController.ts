5873f093d0dd1d3deb5c8bf852c7b4488928c6ac
unknown <dl.kokhtashvili@gmail.com>
2024-03-22 15:00:37 +0400

    Implement password hashing using bcrypt

--- src/controllers/userRegistrationController.ts
+++ src/controllers/userRegistrationController.ts
@@ -2,26 +2,24 @@ import userRegistrationModel from "models/userRegistrationModel";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 
-
 const userRegistrationController = async (req: Request, res: Response) => {
-    const { name, email, password, repeatPassword } = req.body;
+  const { name, email, password, repeatPassword } = req.body;
 
-    if(password !== repeatPassword ) {
-        return res.status(401).json("passwords do not match");
-    }
+  if (password !== repeatPassword) {
+    return res.status(401).json("passwords do not match");
+  }
 
-    try{
-       
-        // const salt = await bcrypt.genSalt(10);
-        // const hashedPassword = await bcrypt.hash(password, salt);
-        
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hashedPassword",hashedPassword)
 
-        const newUser = new userRegistrationModel({ name, email, password });
-        newUser.save();
-        return res.status(201).json("user registered succesfully");
-    }catch(error ){
-        return res.status(401).json(error);
-    }
-}
+    const newUser = new userRegistrationModel({ name, email, password:hashedPassword });
+    newUser.save();
+    return res.status(201).json("user registered succesfully");
+  } catch (error) {
+    return res.status(401).json(error);
+  }
+};
 
 export default userRegistrationController;
